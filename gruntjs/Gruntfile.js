1f06df3a76a5792392097e09b2c630de8201e1e8
Martin <martin.heimsoth@lexparency.de>
2022-06-16 22:22:51 +0200

    Removed ad-panel residuals. Serving jQuery and tippy.js myself, instead of using CDN.

--- gruntjs/Gruntfile.js
+++ gruntjs/Gruntfile.js
@@ -50,17 +50,7 @@ module.exports = function(grunt){
       someTarget: {
         options: {
           rewriter: function (url) {
-            if (url === '/static/js/jquery.min.js') {
-              return 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
-            } else if (url === '/static/js/tippy.all.min.js') {
-              return 'https://unpkg.com/tippy.js@2.5.2/dist/tippy.all.min.js'
-            } else if (url === '/static/js/intro.min.js') {
-              return 'https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/intro.min.js'
-            } else if (url === '/static/css/introjs.min.css') {
-              return 'https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/introjs.min.css'
-            } else {
-              return url  // all others stay the same.
-            }
+            return url  // all others stay the same.
           }
         },
         files: [{
